9439514b8dbf16b419ab0b7186d7da2ecbfab9f1
Malle02 <114107017+Malle02@users.noreply.github.com>
2024-11-17 23:12:37 +0100

    Update Trap.java

--- RPG_Avancer/src/MonPackage/Trap.java
+++ RPG_Avancer/src/MonPackage/Trap.java
@@ -1,21 +1,21 @@
-package MonPackage;
+// package MonPackage;
 
-public class Trap extends Destruction {
-    private static final double DAMAGE = 15;
+// public class Trap extends Destruction {
+//     private static final double DAMAGE = 15;
 
-    public Trap() {
-        super(DAMAGE);
-    }
+//     public Trap() {
+//         super(DAMAGE);
+//     }
 
-    public String asciiArt() {
-        return "💀 Piège\n" +
-               "   _____\n" +
-               "  |     |\n" +
-               "  |  ☠️  |\n" +
-               "  |_____|\n";
-    }
+//     public String asciiArt() {
+//         return "💀 Piège\n" +
+//                "   _____\n" +
+//                "  |     |\n" +
+//                "  |  ☠️  |\n" +
+//                "  |_____|\n";
+//     }
 
-    public double getDamage() {
-        return DAMAGE;
-    }
-}
+//     public double getDamage() {
+//         return DAMAGE;
+//     }
+// }
